c605094ab2e42372a69367e9db0a26c229e76911
ismail <ismailalhazaa@gmail.com>
2026-05-30 00:41:33 +0300

    edit axios instance to add the the production url for backend

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://mocklyai-backend-eeetgjbsdedyfaex.swedencentral-01.azurewebsites.net',
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
